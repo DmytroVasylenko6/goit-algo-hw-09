aa01f51c93cbf0c687bf381d2749b8d4f8154f7e
DmytroVasylenko6 <tredstoun651@ukr.net>
2024-06-08 17:02:35 +0300

    add files

---
+++ main.py
@@ -0,0 +1,115 @@
+import timeit
+
+COINS = [50, 25, 10, 5, 2, 1]
+
+
+def handle_error(func):
+    def wrapper(total, coins):
+        if total <= 0:
+            raise ValueError("Total amount must be a positive integer.")
+        if not coins or any(coin <= 0 for coin in coins):
+            raise ValueError("Coins must be a non-empty list of positive integers.")
+        if total == 0 or min(COINS) > total:
+            return {}
+        return func(total, coins)
+
+    return wrapper
+
+
+@handle_error
+def find_coins_greedy(total, coins):
+    change_coins = {}
+    descending_coins_range = sorted(coins, reverse=True)
+    for coin in descending_coins_range:
+        count = total // coin
+        total -= count * coin
+        if count != 0:
+            change_coins[coin] = count
+
+    return change_coins
+
+
+@handle_error
+def find_min_coins(total, coins):
+    dp = [float("inf")] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    change_coins = {}
+    remaining_total = total
+    for coin in reversed(coins):
+        while (
+            remaining_total - coin >= 0
+            and dp[remaining_total] == dp[remaining_total - coin] + 1
+        ):
+            change_coins[coin] = change_coins.get(coin, 0) + 1
+            remaining_total -= coin
+
+    return change_coins
+
+
+def benchmark(total):
+    greedy_result = timeit.timeit(lambda: find_coins_greedy(total, COINS), number=1000)
+    dynamic_result = timeit.timeit(lambda: find_min_coins(total, COINS), number=1000)
+    return greedy_result, dynamic_result
+
+
+if __name__ == "__main__":
+    greedy_simple, dynamic_simple = benchmark(50)
+    greedy_sm, dynamic_sm = benchmark(67)
+    greedy_md, dynamic_md = benchmark(470)
+    greedy_lg, dynamic_lg = benchmark(8852)
+
+    greedy_sm_coin_combinations = find_coins_greedy(67, COINS)
+    dynamic_sm_coin_combinations = find_coins_greedy(67, COINS)
+    greedy_md_coin_combinations = find_coins_greedy(470, COINS)
+    dynamic_md_coin_combinations = find_min_coins(470, COINS)
+    greedy_lg_coin_combinations = find_min_coins(8852, COINS)
+    dynamic_lg_coin_combinations = find_min_coins(8852, COINS)
+
+    # Results for different exchange amounts made with the greedy algorithm
+    print(
+        "{:<24}".format("Greedy algorithm"),
+        "{:<50}".format(f"Coin combinations: {greedy_sm_coin_combinations}"),
+        f"Time: {greedy_sm:.6f} sec",
+    )
+    print(
+        "{:<24}".format("Greedy algorithm:"),
+        "{:<50}".format(f"Coin combinations: {greedy_md_coin_combinations}"),
+        f"Time: {greedy_md:.6f} sec",
+    )
+    print(
+        "{:<24}".format("Greedy algorithm:"),
+        "{:<50}".format(f"Coin combinations: {greedy_lg_coin_combinations}"),
+        f"Time: {greedy_lg:.6f} sec",
+    )
+    # Results for different exchange amounts made with the dynamic programming algorithm
+    print(
+        "{:<24}".format("Dynamic programming:"),
+        "{:<50}".format(f"Coin combinations: {dynamic_sm_coin_combinations}"),
+        f"Time: {dynamic_sm:.6f} sec",
+    )
+    print(
+        "{:<24}".format("Dynamic programming:"),
+        "{:<50}".format(f"Coin combinations: {dynamic_md_coin_combinations}"),
+        f"Time: {dynamic_md:.6f} sec",
+    )
+    print(
+        "{:<24}".format("Dynamic programming:"),
+        "{:<50}".format(f"Coin combinations: {dynamic_lg_coin_combinations}"),
+        f"Time: {dynamic_lg:.6f} sec",
+    )
+
+    # Simple
+    print(
+        "{:<75}".format("Simple amount (greedy):"),
+        f"Time: {greedy_simple:.6f} sec",
+    )
+    print(
+        "{:<75}".format("Simple amount (dynamic):"),
+        f"Time: {dynamic_simple:.6f} sec",
+    )
